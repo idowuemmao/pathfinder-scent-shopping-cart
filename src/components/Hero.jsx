2075a81bb019aaf1ace71eae6ad7791026b442da
Idowu Emmanuel <idowuemmao@gmail.com>
2023-11-09 17:11:10 +0100

    working on the counter on items

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ShopItemData from "./ShopItemDb";
 
-const Hero = () => {
+const Hero = ({ cart }) => {
   const [count, setCount] = useState(0);
-  const increment = (id) => {
-    setCount((prevCount) => prevCount + 1);
-    console.log(count);
-  };
-  const decrement = (id) => {
-    setCount((prevCount) => prevCount + 1);
-    console.log(count);
-  };
+  // const decrement = (id) => {
+  //   let selectedItem = id;
+  //   let search = count;
+  //   setCount((prevCount) => id + 1);
+  //   console.log(id);
+  // };
+  // const increment = (i) => {
+  //   setCount((prevCount) => i + 1);
+  //   console.log(i);
+  // };
 
-  const shopItems = ShopItemData.map((item) => (
-    <div key={item.id} className="w-36 border-2 border-black flex">
-      <img
-        src={item.img}
-        alt={item.name}
-        className="w-12 object-contain max-h-28"
-      />
-      <div className="p-1">
-        <h2 className="text-xs capitalize font-bold">{item.name}</h2>
-        <p className="text-[0.4rem]">{item.desc}</p>
-        <div className="flex text-[0.5rem] items-center">
-          <h4 className="">${item.price}</h4>
-          <div className="flex gap-1 ml-auto font-semibold">
-            <button onClick={decrement}>-</button>
-            <span>{count}</span>
-            <button onClick={increment}>+</button>
+  const shopItems = ShopItemData.map(
+    ({ id, name, desc, price, initialCount, img }) => (
+      <div key={id} className="w-36 border-2 border-black flex">
+        <img src={img} alt={name} className="w-12 object-contain max-h-28" />
+        <div className="p-1">
+          <h2 className="text-xs capitalize font-bold">{name}</h2>
+          <p className="text-[0.4rem]">{desc}</p>
+          <div className="flex text-[0.5rem] items-center">
+            <h4 className="">${price}</h4>
+            <div className="flex gap-1 ml-auto font-semibold">
+              <button onClick={() => setCount(count + 1)}>-</button>
+              {/* <span ref={countRef}>{ search}</span> */}
+              <span id={`${id}`}>{count}</span>
+              <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  ));
+    )
+  );
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      {cart}
       {shopItems}
     </div>
   );
