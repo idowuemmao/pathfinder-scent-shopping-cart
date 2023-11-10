5acac3cfdf754ff952079f886bbd7b298fc50ea5
Idowu Emmanuel <idowuemmao@gmail.com>
2023-11-10 16:04:04 +0100

    saving on local storage

--- src/components/Perfs.jsx
+++ src/components/Perfs.jsx
@@ -4,45 +4,56 @@ import PerfCard from "./PerfCard";
 
 const Perfs = () => {
   const [items, setItems] = useState(ShopItemData);
+  const storedItems = JSON.parse(localStorage.getItem("items"));
+  useEffect(() => {
+    if (storedItems) {
+      setItems(storedItems);
+    } else {
+      setItems(ShopItemData);
+    }
+  }, []);
 
+  const updateLocalStorage = (updateItems) => {
+    localStorage.setItem("items", JSON.stringify(updateItems));
+  };
   const handleIncrement = (itemId) => {
     setItems((prevItems) => {
-      return prevItems.map((item) => {
+      const updateItems = prevItems.map((item) => {
         if (item.id === itemId) {
           return { ...item, count: item.count + 1 };
         }
         return item;
       });
+      updateLocalStorage(updateItems);
+      return updateItems;
     });
   };
   const handleDecrement = (itemId) => {
     setItems((prevItems) => {
-      return prevItems.map((item) => {
+      const updateItems = prevItems.map((item) => {
         if (item.id === itemId) {
           return { ...item, count: item.count - 1 };
         }
         return item;
       });
+      updateLocalStorage(updateItems);
+      return updateItems;
     });
   };
   return (
     <div className="grid grid-cols-2 gap-4">
-      {items ? (
-        items.map((item) => (
-          <PerfCard
-            key={item.id}
-            name={item.name}
-            count={item.count}
-            price={item.price}
-            img={item.img}
-            desc={item.desc}
-            onIncrement={() => handleIncrement(item.id)}
-            onDecrement={() => handleDecrement(item.id)}
-          />
-        ))
-      ) : (
-        <PerfCard />
-      )}
+      {items.map((item) => (
+        <PerfCard
+          key={item.id}
+          name={item.name}
+          count={item.count}
+          price={item.price}
+          img={item.img}
+          desc={item.desc}
+          onIncrement={() => handleIncrement(item.id)}
+          onDecrement={() => handleDecrement(item.id)}
+        />
+      ))}
     </div>
   );
 };
