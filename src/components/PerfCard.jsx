03d45d8ad2dfb5cf1c0ca3b8148b6a324a31c911
Idowu Emmanuel <idowuemmao@gmail.com>
2023-11-15 12:16:07 +0100

    removed the default popup appearing on page render

--- src/components/PerfCard.jsx
+++ src/components/PerfCard.jsx
@@ -12,7 +12,7 @@ const PerfCard = ({
   onIncrement,
   onDecrement,
 }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const closeModal = () => {
     setIsOpen(false);
   };
